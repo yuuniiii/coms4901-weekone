18e325f75f651424d2edd5a75ff9fdc688edb22e
Yuni Chun <yunichun@gmail.com>
2026-02-03 19:37:53 -0500

    connected to supabase, made image table list page

---
+++ app/list/page.tsx
@@ -0,0 +1,24 @@
+import { supabase } from '@/lib/supabaseClient'
+
+export default async function ListPage() {
+  const { data, error } = await supabase
+    .from('images')
+    .select('*')
+
+  if (error) {
+    return <div>Error loading data</div>
+  }
+
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Supabase Data (images table) </h1>
+      <ul>
+        {data?.map((row: any) => (
+          <li key={row.id}>
+            {JSON.stringify(row)}
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
