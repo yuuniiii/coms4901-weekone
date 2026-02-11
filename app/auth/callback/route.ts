cd7c57891143b803c6ddf34726cfb90459945ae9
Yuni Chun <yunichun@gmail.com>
2026-02-10 19:28:47 -0500

    troubleshooting again

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get("code")
 
-  const cookieStore = cookies()
+  const cookieStore = await cookies() // ✅ THIS is the key
 
   if (code) {
     const supabase = createServerClient(
