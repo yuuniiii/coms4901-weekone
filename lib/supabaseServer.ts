4ebd040fa89f9caf500280951be4a3a6c914c255
Yuni Chun <yunichun@gmail.com>
2026-02-10 19:44:00 -0500

    deleted supabaseserver file

--- lib/supabaseServer.ts
+++
@@ -1,9 +0,0 @@
-import { createServerClient } from "@supabase/ssr"
-
-export function createSupabaseServerClient() {
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {}
-  )
-}
