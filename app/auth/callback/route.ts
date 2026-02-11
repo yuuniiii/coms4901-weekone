de3fa838ac904fec1fbe8692268c0121e304f277
Yuni Chun <yunichun@gmail.com>
2026-02-10 19:18:51 -0500

    forget about cookies

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,26 +1,14 @@
 import { NextResponse } from "next/server"
 import { createServerClient } from "@supabase/ssr"
-import { cookies } from "next/headers"
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get("code")
 
-  const cookieStore = await cookies()
-
   if (code) {
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          get: (name) => cookieStore.get(name)?.value,
-          set: (name, value, options) =>
-            cookieStore.set({ name, value, ...options }),
-          remove: (name, options) =>
-            cookieStore.set({ name, value: "", ...options }),
-        },
-      }
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     )
 
     await supabase.auth.exchangeCodeForSession(code)
