9201576feae1b00ebc494cef4a63a7332b43d161
Yuni Chun <yunichun@gmail.com>
2026-02-10 19:21:09 -0500

    removing more cookies code

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -5,13 +5,7 @@ export function createSupabaseServerClient() {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get: (name) => cookies().get(name)?.value,
-        set: (name, value, options) =>
-          cookies().set({ name, value, ...options }),
-        remove: (name, options) =>
-          cookies().set({ name, value: "", ...options }),
+    {}),
       },
     }
   )
