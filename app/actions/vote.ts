a21294409cf8351fcf86178f07c213a462965fab
Yuni Chun <yunichun@gmail.com>
2026-02-18 15:05:14 -0500

    trying to find error

--- app/actions/vote.ts
+++ app/actions/vote.ts
@@ -51,7 +51,7 @@ export async function submitVote(captionId: string, voteValue: number) {
 
   if (error) {
     console.error(error)
-    throw new Error("Vote failed")
+    return
   }
 
   revalidatePath("/feed")
